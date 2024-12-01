3ecbda6ceb7287a512bcf1df5a2bf2d79a1760b3
abhishek <abhishekpal2500@gmail.com>
2024-12-01 18:41:57 +0530

    upload folder add

--- config/plugins.js
+++ config/plugins.js
@@ -5,6 +5,9 @@ module.exports = ({ env }) => ({
         cloud_name: env('CLOUDINARY_CLOUD_NAME'),
         api_key: env('CLOUDINARY_API_KEY'),
         api_secret: env('CLOUDINARY_API_SECRET'),
+        folder: 'pbg_uploads',
+        use_filename: true, // Whether to use the original filename
+        unique_filename: true,
         sizeLimit: 5000000,
         debug: true,
       },
