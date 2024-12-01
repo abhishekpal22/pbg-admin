5df3d3532f00d5cad0e15215f33ab386cb9f9456
abhishek <abhishekpal2500@gmail.com>
2024-12-01 17:52:20 +0530

    update plugin

--- config/plugins.js
+++ config/plugins.js
@@ -5,6 +5,8 @@ module.exports = ({ env }) => ({
         cloud_name: env('CLOUDINARY_CLOUD_NAME'),
         api_key: env('CLOUDINARY_API_KEY'),
         api_secret: env('CLOUDINARY_API_SECRET'),
+        sizeLimit: 5000000,
+        debug: true,
       },
     },
   });
